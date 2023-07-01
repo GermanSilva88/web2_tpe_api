d26d7e9d314a2d5d97ed9c747e9aece96b831db4
Juan Pablo Ostertag <figuta@gmail.com>
2023-07-01 14:25:19 -0300

    Creamos api files

---
+++ api_comment_controller/api_command_controller.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'models/game_model.php';
+
+class api_comment_controller {
+
+
+}
--- route-api.php
+++ route-api.php
@@ -1,11 +1,15 @@
 <?php
+
 require_once 'libs/Router.php';
+
 // crea el router
 $router = new Router();
+
 // define la tabla de ruteo
-$router->addRoute('games', 'GET', 'ApiTaskController', 'obtenerTareas');
-$router->addRoute('games', 'POST', 'ApiTaskController', 'crearTarea');
-$router->addRoute('games/:ID', 'GET', 'ApiTaskController', 'obtenerTarea');
-$router->addRoute('games/:ID', 'DELETE', 'ApiTaskController', 'borrarTarea');
+$router->addRoute('comment', 'GET', 'api_comment_controller', 'get_comments');
+$router->addRoute('comment', 'POST', 'api_comment_controller', 'add_comment');
+$router->addRoute('comment/:ID', 'GET', 'api_comment_controller', 'get_comment');
+$router->addRoute('comment/:ID', 'DELETE', 'api_comment_controller', 'delete_comment');
+
 // rutea
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
