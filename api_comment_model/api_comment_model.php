7295e449a3c78e1163d2c02d977f26ed90a5aabd
Juan Pablo Ostertag <figuta@gmail.com>
2023-07-01 18:12:45 -0300

    modifico model

--- api_comment_model/api_comment_model.php
+++ api_comment_model/api_comment_model.php
@@ -11,18 +11,28 @@ class api_comment_model
     }
 
     public function get_comments()
-    {}
+    {
+
+    }
 
     public function get_comment()
-    {}
+    {
+
+    }
 
     public function save_comment()
-    {}
+    {
+
+    }
 
     public function delete_comment()
-    {}
+    {
+
+    }
 
     public function update_comment()
-    {}
+    {
+        
+    }
 
 }
