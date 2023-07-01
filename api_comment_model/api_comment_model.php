bfc338ed3a70e8bf91e43bd45de4dae9f440f452
Juan Pablo Ostertag <figuta@gmail.com>
2023-07-01 18:34:37 -0300

    agrego mas model

--- api_comment_model/api_comment_model.php
+++ api_comment_model/api_comment_model.php
@@ -12,27 +12,31 @@ class api_comment_model
 
     public function get_comments()
     {
+        $query = $this->db->prepare('SELECT game.name_game, comment.comment_game, comment.score_game FROM game join comment on game.id_game=comment.game_id');
+        $query->execute();
+        $comments = $query->fetchAll(PDO::FETCH_OBJ);
+        return $comments;
 
     }
 
-    public function get_comment()
+    public function get_comment($comment_id)
     {
 
     }
 
-    public function save_comment()
+    public function add_comment($comment, $score, $game_id)
     {
 
     }
 
-    public function delete_comment()
+    public function delete_comment($comment_id)
     {
 
     }
 
-    public function update_comment()
+    public function update_comment($comment_id)
     {
-        
+
     }
 
 }
