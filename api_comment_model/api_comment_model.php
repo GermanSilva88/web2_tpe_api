96443ae0e959982c2d42ac19ef13f641f8271ced
GermanSilva88 <130848764+GermanSilva88@users.noreply.github.com>
2023-07-01 18:07:15 -0300

    mos

--- api_comment_model/api_comment_model.php
+++ api_comment_model/api_comment_model.php
@@ -3,26 +3,20 @@
 class api_comment_model
 {
 
-    private $db;
 
-    function __construct()
-    {
-        $this->db = new PDO('mysql:host=localhost;' . 'dbname=db_esports;charset=utf8', 'root', '');
-    }
 
-    public function get_comments()
-    {}
 
-    public function get_comment()
-    {}
 
-    public function save_comment()
-    {}
 
-    public function delete_comment()
-    {}
 
-    public function update_comment()
-    {}
 
+
+
+
+
+
+
+    public function get_comments(){
+
+}
 }
