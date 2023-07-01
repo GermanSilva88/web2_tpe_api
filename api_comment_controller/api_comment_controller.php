837bb771ccf06b36e3a8e88b5ba136c4a3f4f45e
GermanSilva88 <130848764+GermanSilva88@users.noreply.github.com>
2023-07-01 17:14:16 -0300

    agregamos funcionalidad

--- api_comment_controller/api_comment_controller.php
+++ api_comment_controller/api_comment_controller.php
@@ -4,31 +4,70 @@ define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'
 require_once 'api_comment_model/api_comment_model.php';
 require_once 'api_comment_view/api_comment_view.php';
 
-class api_comment_controller {
+abstract class api_comment_controller
+{
 
-    private $model;
-    private $view;
-    private $data;
+    protected $model;
+    protected $view;
+    protected $data;
 
-    public function __construct (){
+    public function __construct()
+    {
         $this->model = new api_comment_model();
         $this->view = new api_comment_view();
         $this->data = file_get_contents("php://input");
     }
 
-    public function get_comments(){
+    function getData()
+    {
+        return json_decode($this->data);
+    }
+
+    function get($params = [])
+    {
+        if (empty($params)) {
+            //get comments from model
+            $comments = $this->model->get_comments();
+            return $this->view->response($comments, 200);
+        } else {
+            $comment = $this->model->get_comment($params[":ID"]);
+            if (!empty($comment)) {
+                return $this->view->response($comment, 200);
+            } else {
+                return $this->view->response($comment, 404);
+            }
+        }
+    }
+
+    public function add_comment()
+    {
         //get comments from model
     }
 
-    public function add_comment(){
-        //get comments from model
+    public function add_comment($params = [])
+    {
+        // devuelve el objeto JSON enviado por POST
+        $body = $this->getData();
+        // inserta el comentario
+        $comment = $body->comment_game;
+        $score = $body->score_game;
+        $new_comment = $this->model->save_comment($comment, $score);
     }
 
-    public function get_comment($id){
+    public function get_comment($id)
+    {
         //get comment from model by ID
     }
 
-    public function delete_comment($id){
+    public function delete_comment($params = [])
+    {
         //delete comments from model by ID
+        $comment_id = $params[':ID'];
+        $comment = $this->model->get_comment($comment_id);
+        if (!empty($comment)) {
+            $this->model->delete_comment($comment_id);
+            $this->view->response("Comment id=$comment_id eliminada con éxito", 200);
+        } else
+            $this->view->response("Comment id=$comment_id not found", 404);
     }
 }
