c983c9a23a1a3130b3a377dfa11e186ec74a246d
GermanSilva88 <130848764+GermanSilva88@users.noreply.github.com>
2023-07-02 20:17:20 -0300

    implementando modelo

--- api_comment_controller/api_comment_controller.php
+++ api_comment_controller/api_comment_controller.php
@@ -4,12 +4,13 @@ define('BASE_URL', '//' . $_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'
 require_once 'api_comment_model/api_comment_model.php';
 require_once 'api_comment_view/api_comment_view.php';
 
-abstract class api_comment_controller
+class api_comment_controller
 {
 
-    protected $model;
-    protected $view;
-    protected $data;
+    private $model;
+    private $view;
+    private $data;
+
 
     public function __construct()
     {
@@ -18,19 +19,19 @@ abstract class api_comment_controller
         $this->data = file_get_contents("php://input");
     }
 
-    function getData()
+    public function getData()
     {
         return json_decode($this->data);
     }
 
-    function get_com($params = [])
+    public function get_com($params = [])
     {
         if (empty($params)) {
             //get comments from model
             $comments = $this->model->get_comments();
             return $this->view->response($comments, 200);
         } else {
-            $comment_id=$params[":ID"];
+            $comment_id = $params[":ID"];
             $comment = $this->model->get_comment($comment_id);
             if (!empty($comment)) {
                 return $this->view->response($comment, 200);
@@ -42,16 +43,19 @@ abstract class api_comment_controller
 
     public function add_com($params = [])
     {
+
         // devuelve el objeto JSON enviado por POST
         $body = $this->getData();
         // inserta el comentario
         $comment = $body->comment_game;
         $score = $body->score_game;
-        $id_game = $body->game_id; // PREGUNTAR EN TUTORÍA
-        $new_comment = $this->model->add_comment($comment, $score, $id_game);
+        $id_game = $body->game_id;
+
+        $game = $this->model->add_comment($comment, $score, $id_game);
+        $this->view->response("Comentario del juego " . $game->name_game . " creado con éxito", 201);
     }
 
-    public function delete_comment($params = [])
+    public function delete_com($params = [])
     {
         //delete comments from model by ID
         $comment_id = $params[':ID'];
@@ -60,7 +64,7 @@ abstract class api_comment_controller
             $this->model->delete_comment($comment_id);
             $this->view->response("Comment id=$comment_id eliminada con éxito", 200);
         } else
-            $this->view->response("Comment id=$comment_id not found", 404);
+            $this->view->response("Comment id=$comment_id No se encontro", 404);
     }
 
     public function update_com($params = [])
@@ -72,9 +76,24 @@ abstract class api_comment_controller
             $comment = $body->comment_game;
             $score = $body->score_game;
             $id_game = $body->game_id;
-            $comment = $this->model->update_comment($comment, $score, $id_game);
-            $this->view->response("Tarea id=$comment_id actualizada con éxito", 200);
+            $comment = $this->model->update_comment($comment_id, $comment, $score, $id_game);
+            $this->view->response("Comment id=$comment_id actualizada con éxito", 200);
         } else
-            $this->view->response("Task id=$comment_id not found", 404);
+            $this->view->response("Comment id=$comment_id No se encontro", 404);
+    }
+
+    public function get_com_by_order($params = null)
+    {
+        $sort = $_REQUEST['sort'];
+        $order = $_REQUEST['order'];
+        $validSortOptions = ['name_game', 'comment_game', 'score_game'];
+        $validOrderOptions = ['ASC', 'DESC'];
+        if (!in_array($sort, $validSortOptions) || !in_array($order, $validOrderOptions)) {
+            // Manejar el error o establecer valores predeterminados
+            $sort = 'score_game';
+            $order = 'ASC';
+        }
+        $comment_ord=$this->model->get_comment_by_order($sort, $order);
+        return $this->view->response($comment_ord, 200);
     }
 }
--- api_comment_model/api_comment_model.php
+++ api_comment_model/api_comment_model.php
@@ -6,9 +6,9 @@ class api_comment_model
 
     function __construct()
     {
-       $this->db = new PDO('mysql:host=localhost;' . 'dbname=db_esports;charset=utf8', 'root', '');
+        $this->db = new PDO('mysql:host=localhost;' . 'dbname=db_esports;charset=utf8', 'root', '');
     }
-    
+
     public function get_comments()
     {
         $query = $this->db->prepare('SELECT game.name_game, comment.comment_game, comment.score_game FROM game join comment on game.id_game=comment.game_id');
@@ -25,10 +25,14 @@ class api_comment_model
         return $comment;
     }
 
-    public function add_comment($comment, $score, $game_id)
+    public function add_comment($comment, $score, $id_game)
     {
         $query = $this->db->prepare('INSERT INTO comment (comment_game, score_game, game_id) VALUES (?,?,?)');
-        $query->execute(array($comment, $score, $game_id));
+        $query->execute(array($comment, $score, $id_game));
+        $query = $this->db->prepare('SELECT * FROM game WHERE id_game=?');
+        $query->execute(array($id_game));
+        $game = $query->fetch(PDO::FETCH_OBJ);
+        return $game;
     }
 
     public function delete_comment($comment_id)
@@ -37,12 +41,22 @@ class api_comment_model
         $query->execute(array($comment_id));
     }
 
-    public function update_comment($comment_id, $comment, $score)
+    public function update_comment($comment_id, $comment, $score, $id_game)
     {
-        $query = $this->db->prepare('UPDATE comment SET name_game = ?, description_game = ?, genre_id = ? WHERE id_game = ?');
-        $query->execute(array($name_game, $description_game,$id_genre, $id));
+        $query = $this->db->prepare('UPDATE comment SET comment_game = ?, score_game = ?, game_id = ? WHERE id = ?');
+        $query->execute(array($comment, $score, $id_game, $comment_id));
     }
 
-
- 
+    public function get_comment_by_order($sort, $order)
+    {
+        $query = $this->db->prepare('SELECT game.name_game, comment.comment_game, comment.score_game 
+        FROM game 
+        JOIN comment ON game.id_game = comment.game_id 
+        ORDER BY ' . $sort . ' ' . $order);
+        //  $query->bindParam(':sort', $sort);
+        //  $query->bindParam(':order', $order);
+         $query->execute();
+        $comments = $query->fetchAll(PDO::FETCH_OBJ);
+        return $comments;
+    }
 }
